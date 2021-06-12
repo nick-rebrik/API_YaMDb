2d49c51efc0e7bbbc981f2d168c82e76e49fb459
Nick Rebrik <nick.rebrik@gmail.com>
2021-06-12 22:12:04 +0200

    fix admin

--- api/admin.py
+++ api/admin.py
@@ -3,14 +3,7 @@ from django.contrib import admin
 from .models import Category, Genre, Reviews, Title
 
 
-class TitleAdmin(admin.ModelAdmin):
-    list_display = ('pk', 'name', 'category', 'genre', 'year')
-    search_fields = ('name',)
-    list_filter = ('year',)
-    empty_value_display = '-пусто-'
-
-
 admin.site.register(Category)
 admin.site.register(Genre)
 admin.site.register(Reviews)
-admin.site.register(Title, TitleAdmin)
+admin.site.register(Title)
