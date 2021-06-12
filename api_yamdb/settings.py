524e56973cad3aaeafb316fdc0ba97f952e0483c
Nick Rebrik <nick.rebrik@gmail.com>
2021-06-12 14:31:13 +0200

    изменение settings файла

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 
-AUTH_USER_MODEL = 'models.MyUser'
+AUTH_USER_MODEL = 'api.MyUser'
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
