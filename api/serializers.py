b05d561578263faf310e1d9f69e1424d20a27da8
Nick Rebrik <nick.rebrik@gmail.com>
2021-06-12 18:19:38 +0200

    add urls, add serializers, add view | model Review, Comments

---
+++ api/serializers.py
@@ -0,0 +1,27 @@
+from rest_framework import serializers
+
+from .models import Comments, Reviews
+
+
+class ReviewSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Reviews
+
+
+class CommentSerializer(serializers.ModelSerializer):
+    author = serializers.SlugRelatedField(
+        read_only=True,
+        slug_field='username',
+        default=serializers.CurrentUserDefault()
+    )
+
+    class Meta:
+        fields = '__all__'
+        model = Comments
