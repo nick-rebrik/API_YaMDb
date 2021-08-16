49ca1163e628d5fb153801b608893b06c6c8ce53
nick-rebrik <77996995+nick-rebrik@users.noreply.github.com>
2021-08-16 21:27:22 +0200

    Update settings.py

--- api_yamdb/settings.py
+++ api_yamdb/settings.py
@@ -10,7 +10,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p&l%385148kslhtyn^##a1)ilz@4zqj=rq&agdol^##zgl9(vs'
+SECRET_KEY = 'Your KEY'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
